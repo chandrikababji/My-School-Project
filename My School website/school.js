cac63bb87846cde321231c2816daa9e335f9ebd3
Saichandrika Kanaparthi <157704159+chandrikababji@users.noreply.github.com>
2024-11-11 17:32:46 +0530

    Add files via upload

---
+++ My School website/school.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you for reaching out! We'll get back to you soon.");
+    document.getElementById("contactForm").reset();
+});
